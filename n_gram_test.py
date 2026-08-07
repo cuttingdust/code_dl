b277ab42324f284c1f7061c15d0fe48f83c7f44f
handabao <18862963156@163.com>
2026-08-07 18:10:57 +0800

    01-特征预处理_n_gram

---
+++ n_gram_test.py
@@ -0,0 +1,26 @@
+def n_gram_fn(n_gram):
+    # 1- 测试数据
+    word_list = ["aa", "bb", "cc", "dd"]
+
+    # 2- 对相邻的词进行合并
+    """
+        2-gram：
+            ["aa","bb","cc","dd"]
+            ["bb","cc","dd"]
+
+        3-gram：
+            ["aa","bb","cc","dd"]
+            ["bb","cc","dd"]
+            ["cc","dd"]
+    """
+    lists = [word_list[i:] for i in range(n_gram)]
+    print(list(zip(*lists)))
+
+
+if __name__ == "__main__":
+    n_gram_fn(n_gram=1)
+    print("-" * 50)
+    n_gram_fn(n_gram=2)
+    print("-" * 50)
+    n_gram_fn(n_gram=3)
+    print("-" * 50)
