bcce029edfb86ffd742524237bae6be4d093aaa6
handabao <18862963156@163.com>
2026-09-16 11:22:59 +0800

    feat: add teacher and BiLSTM student models

---
+++ 07--tmf/05--distill/config.py
@@ -0,0 +1,41 @@
+import torch
+
+
+class Config:
+    def __init__(self):
+        # 1- 设备
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # 2- 原始文件路径
+        self.train_datapath = "data/train.txt"
+        self.dev_datapath = "data/dev.txt"
+        self.test_datapath = "data/test.txt"
+        self.class_datapath = "data/class.txt"
+
+        # 3- 数据加载器参数
+        self.batch_size = 64  # 64条新闻
+        self.max_length = 32  # 句子中词的个数最多是32个词
+
+        # 4- Bert预训练模型的路径
+        self.bert_path = r"..\..\PretrainedModel\bert-base-chinese"
+
+        # 5- 目标值文件解析
+        self.classname_list = [
+            line.strip()
+            for line in open(self.class_datapath, mode="r", encoding="UTF-8")
+        ]
+        self.classname_len = len(self.classname_list)
+
+        # 6- 训练好的【教师模型】的保存路径
+        # 当前工作目录是05--distill；教师模型已经由03--bert训练并保存，
+        # 因此先返回上一级07--tmf，再进入03--bert读取教师模型权重。
+        self.teacher_save_model = r"save_model\bert.pkl"
+
+        # 7- 训练好的【学生模型】的保存路径
+        self.student_save_model = r"save_model\student_bert.pkl"
+
+
+if __name__ == "__main__":
+    config = Config()
+    print(config.classname_list)
+    print(config.classname_len)
